35a71d8b43ca8bcc621920d73296eea30e559967
Akash Chaudhari <138999370+akashch1512@users.noreply.github.com>
2024-07-03 17:45:34 +0530

    Add files via upload

---
+++ src/images/index.ts
@@ -0,0 +1,17 @@
+import bear from "./bear.png";
+import coin from "./coin.png";
+import highVoltage from "./high-voltage.png";
+import notcoin from "./notcoin.png";
+import rocket from "./rocket.png";
+import trophy from "./trophy.png";
+import Genie from "./sample_genie_lamp.png";
+
+export {
+    bear,
+    coin,
+    highVoltage,
+    notcoin,
+    rocket,
+    trophy,
+    Genie
+}
