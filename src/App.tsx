b92382e30b90db67484cadf24eeeff0e5fe669e5
Akash Chaudhari <138999370+akashch1512@users.noreply.github.com>
2024-07-04 15:37:18 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,7 @@ const App = () => {
 
       <div className="w-full z-10 min-h-screen flex flex-col items-center text-white">
 
-        <div className="fixed top-1 left-0 w-full px-4 pt-8 z-10 flex flex-col items-center text-white">
+        <div className="fixed top-15 left-0 w-full px-4 pt-8 z-10 flex flex-col items-center text-white">
           {/* <div className="w-full cursor-pointer"> */}
             {/* <div className="bg-[#1f1f1f] text-center py-2 rounded-xl"> */}
               {/* <p className="text-lg">Join squad <Arrow size={18} className="ml-0 mb-1 inline-block" /></p> */}
